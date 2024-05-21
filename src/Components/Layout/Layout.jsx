3ada3d43d64fdc5cd5f178cacea52a35614c7c2e
ho3einHamidi <hosseinmhh@yahoo.com>
2024-05-21 22:24:59 +0330

    feat: header and footer layout

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import styles from "./Layout.module.css";
+function Layout() {
+  return (
+    <>
+      <header>
+        <h2>Crypto App</h2>
+      </header>
+
+      <footer>Developed By Hossein Hamidi</footer>
+    </>
+  );
+}
+
+export default Layout;
